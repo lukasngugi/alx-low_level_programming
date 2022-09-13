885344a2cf8066b364c682cbb4351e37070a9c3e
lukasngugi <nguginguku@gmail.com>
2022-09-13 08:10:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet - Printing small lettets
+ *
+ *
+ *
+ * Return: void
+ *
+*/
+
+void print_alphabet(void)
+
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
