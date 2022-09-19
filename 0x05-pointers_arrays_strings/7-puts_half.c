c0db82976a693e1135f89f29b27d5e3da32776ec
lukasngugi <nguginguku@gmail.com>
2022-09-19 10:44:22 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: a string
+ *
+ * Return: void
+ *
+*/
+
+void puts_half(char *str)
+
+{
+	unsigned long int i;
+
+	n = strlen(str) / 2;
+
+	len = strlen(str);
+
+	n = len / 2;
+
+	for (i = n; i <= strlen(str); i++)
+	{
+		if (len % 2 == 0)
+			_putchar(str[i]);
+		else if (len % 2 == 1)
+			_putchar(str[i + 1]);
+	}
+	_putchar('\n');
+}
