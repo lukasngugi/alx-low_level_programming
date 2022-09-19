c695656d2cf2cb25a498c26eae692de9833bf2df
lukasngugi <nguginguku@gmail.com>
2022-09-19 09:20:08 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -4,7 +4,8 @@
  * main - check the code 
  *
  *
- *turn: Always 0.
+ *
+ * Return: Always 0.
  *
 */
 
@@ -12,10 +13,10 @@ int main(void)
 
 {
     int n;
-
-        n = 402;
+    
+    n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
-	return (0);
+    return (0);
 }
